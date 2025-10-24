bede543f97b490a6e08d231901dd4796b529321c
zunaira0042 <zunaira.nazir30@gmail.com>
2025-10-24 21:11:44 +0500

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -69,7 +69,7 @@ const Projects = () => {
       impact:
         "An engaging, immersive first-person experience optimized for PC users, highlighting strong environmental design and interactive gameplay.",
       icon: <Gamepad2 className="w-6 h-6 text-electric" />,
-      status: "Liveeee",
+      status: "Live",
       link: "https://apps.microsoft.com/detail/9ppgs21dkcck?hl=en-US&gl=US", // optional demo link
     },
   ];
